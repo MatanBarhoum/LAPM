657609d6d0a98d90a8b0d883404a2c16774f770a
Matan Barhoum <88832406+MatanBarhoum@users.noreply.github.com>
2025-07-24 20:05:08 +0300

    Update app.component.ts

--- Front/LAPM-Frontend/src/app/app.component.ts
+++ Front/LAPM-Frontend/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
-import { NavbarComponent } from './components/navbar/navbar.component'; // <-- IMPORT
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent], // <-- ADD NavbarComponent
+  imports: [CommonModule, RouterOutlet, NavbarComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
